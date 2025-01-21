e836aa875dbb2c5a25742c7194e07ed0c8b5efc3
Pepedik <s2300864@edu.bc.fi>
2025-01-21 12:01:08 +0200

    tehty

---
+++ DVD_logo_testi/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+namespace DVD_logo_testi
+{
+    internal class Program
+    {
+        
+        
+        
+        static void Main(string[] args)
+        {
+            string logo = "DVD";
+            Vector2 Dlogo = new Vector2(1, 0);
+            Vector2 Adir = new Vector2(1, 1);
+
+            int width = 800;
+            int height = 800;
+
+            float speed = 100.0f;
+
+            Raylib.InitWindow(width, height, "DVD_Logo");
+            while (Raylib.WindowShouldClose() == false)
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                Vector2 TextSize = Raylib.MeasureTextEx(Raylib.GetFontDefault(), logo, 32, 2);
+                Raylib.DrawText(logo, (int)Dlogo.X, (int)Dlogo.Y, 32, Color.Yellow);
+
+                Raylib.EndDrawing();
+
+                Dlogo = Dlogo + Adir * speed * Raylib.GetFrameTime();
+
+                if (Dlogo.X < 0 || Dlogo.X + TextSize.X > width)
+                {
+                    Adir.X *= -1;
+                }
+                if (Dlogo.Y < 0 || Dlogo.Y + TextSize.Y > height)
+                {
+                    Adir.Y *= -1;
+                }
+
+            }
+            Raylib.CloseWindow();
+        }
+
+    }
+}
---
+++ Reylib_Testi/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+namespace Reylib_Testi
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 800;
+            int width = 800;
+            Vector2 A = new Vector2(width/ 2, 0);
+            Vector2 B = new Vector2(0, height / 2);
+            Vector2 C = new Vector2(width, height * 3 / 4);
+
+            Vector2 Amove = new Vector2(1, 1);
+            Vector2 Bmove = new Vector2(1, -1);
+            Vector2 Cmove = new Vector2(-1, -1);
+            
+
+            Raylib.InitWindow(width, height, "Raylibtesti");
+            while (Raylib.WindowShouldClose() == false)
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+
+                Raylib.DrawLineV(A, B, Color.Green);
+                Raylib.DrawLineV(B, C, Color.Yellow);
+                Raylib.DrawLineV(C, A, Color.SkyBlue);
+                Raylib.EndDrawing();
+
+                A = A + Amove * 100 * Raylib.GetFrameTime();
+                B = B + Bmove * 100 * Raylib.GetFrameTime();
+                C = C + Cmove * 100 * Raylib.GetFrameTime();
+
+                if (A.X>=width || A.X <= 0)
+                {
+                    Amove.X = Amove.X * -1;
+                }
+                if (B.X >= width || B.X <= 0)
+                {
+                    Bmove.X = Bmove.X * -1;
+                }
+                if (C.X >= width || C.X <= 0)
+                {
+                    Cmove.X = Cmove.X * -1;
+                }
+                
+                if (A.Y >= height || A.Y <= 0)
+                {
+                    Amove.Y = Amove.Y * -1;
+                }
+                if (B.Y >= height || B.Y <= 0)
+                {
+                    Bmove.Y = Bmove.Y * -1;
+                }
+                if (C.Y >= height || C.Y <= 0)
+                {
+                    Cmove.Y = Cmove.Y * -1;
+                }
+
+            }
+            Raylib.CloseWindow();
+        }
+    }
+}
