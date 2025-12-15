67cd83959aeef8c3b597530510686bfec16b6830
Pepedik <s2300864@edu.bc.fi>
2025-12-15 14:26:49 +0200

    2Comments

--- Asteroids/Asteroid.cs
+++ Asteroids/Asteroid.cs
@@ -12,7 +12,12 @@ namespace Asteroids
         public static Random rng = new Random();
         public float rad;
 
-
+        /// <summary>
+        /// This creates new asteroid.
+        /// </summary>
+        /// <param name="RandomPos">Random place</param>
+        /// <param name="size">Size should be atleast 1</param>
+        /// <param name="Texture">Texture has to match the size</param>
         public Asteroids(Vector2 RandomPos, int size, Texture2D Texture)
         {
             int screenW = Raylib.GetScreenWidth();
